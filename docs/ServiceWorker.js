05d85aa27ab47649680bf47b1e5279d2849c3a8d
AppDani <Danielarog10@gmail.com>
2026-07-27 18:46:03 +0300

    fixes

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DanielaRog-allergyNinja-1.1";
+const cacheName = "DanielaRog-allergyNinja-1.2";
 const contentToCache = [
     "Build/docs.loader.js",
     "Build/docs.framework.js",
